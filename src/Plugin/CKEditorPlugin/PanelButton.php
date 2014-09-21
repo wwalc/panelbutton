bf17c0a672887a2a0dc199f643296a0d4297f4c3
Wiktor Walc <w.walc@cksource.com>
2014-09-21 21:14:17 +0200

    Code Cleanup

--- src/Plugin/CKEditorPlugin/PanelButton.php
+++ src/Plugin/CKEditorPlugin/PanelButton.php
@@ -2,26 +2,17 @@
 
 /**
  * @file
- * Contains \Drupal\panelbutton\Plugin\CKEditorPlugin\PanelButton.
+ * Definition of \Drupal\panelbutton\Plugin\CKEditorPlugin\PanelButton.
  */
 
 namespace Drupal\panelbutton\Plugin\CKEditorPlugin;
 
 use Drupal\ckeditor\CKEditorPluginInterface;
 use Drupal\Component\Plugin\PluginBase;
-use Drupal\ckeditor\Annotation\CKEditorPlugin;
-// Do not remove the line below because of:
-// [Semantical Error] The annotation "@Translation" in class
-// Drupal\ckeditor_smiley\Plugin\CKEditorPlugin\Smiley was never imported.
-// Did you maybe forget to add a "use" statement for this annotation?
-use Drupal\Core\Annotation\Translation;
 use Drupal\editor\Entity\Editor;
 
 /**
  * Defines the "PanelButton" plugin.
- * @see MetaContextual
- * @see MetaButton
- * @see MetaContextualAndButton
  *
  * @CKEditorPlugin(
  *   id = "panelbutton",
